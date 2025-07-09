4fcdb99f5eeab4acfe8465e9cbdf1fb4da3598c0
strangekid7822 <keji_zhaoqiang2006@163.com>
2025-07-09 16:32:52 +0800

    feat: update nav bar background to secondary color

--- src/components/WordTestNavBar.jsx
+++ src/components/WordTestNavBar.jsx
@@ -1,6 +1,6 @@
 function WordTestNavBar() {
   return (
-    <div className="flex justify-around items-center p-4 bg-[var(--color-grey)] rounded-full shadow-lg">
+    <div className="flex justify-around items-center p-4 bg-secondary rounded-full shadow-lg">
       <a href="/" className="text-[var(--color-black)]">Home</a>
       <a href="/write" className="text-[var(--color-primary)] font-bold">Write</a>
       <a href="/profile" className="text-[var(--color-black)]">Profile</a>
