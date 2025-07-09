4a306101ad06f6b391fa358df45b3197f21d17e3
strangekid7822 <keji_zhaoqiang2006@163.com>
2025-07-09 16:48:16 +0800

    Replace navbar text with SVG icons
    - Use SVG icons from public folder for cleaner UI
    - Maintain secondary color background as intended
    - Add proper alt text for accessibility

--- src/components/WordTestNavBar.jsx
+++ src/components/WordTestNavBar.jsx
@@ -1,9 +1,15 @@
 function WordTestNavBar() {
   return (
-    <div className="flex justify-around items-center p-4 bg-secondary rounded-full shadow-lg">
-      <a href="/" className="text-[var(--color-black)]">Home</a>
-      <a href="/write" className="text-[var(--color-primary)] font-bold">Write</a>
-      <a href="/profile" className="text-[var(--color-black)]">Profile</a>
+    <div className="flex justify-around items-center p-4 bg-[var(--color-secondary)] rounded-full shadow-lg">
+      <a href="/" className="p-2">
+        <img src="/WordTestNavBar_HomeButton.svg" alt="Home" className="w-6 h-6" />
+      </a>
+      <a href="/write" className="p-2">
+        <img src="/WordTestNavBar_WordCardButton.svg" alt="Write" className="w-6 h-6" />
+      </a>
+      <a href="/profile" className="p-2">
+        <img src="/WordTestNavBar_UserProfileButton.svg" alt="Profile" className="w-6 h-6" />
+      </a>
     </div>
   );
 }
