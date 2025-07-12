7218f9d420369cb235e7b53b97131181b4d31515
strangekid7822 <strangekid7822@users.noreply.github.com>
2025-07-12 10:24:33 +0800

    Make the gallery ignore the left and right paddings.

--- src/pages/WordTest.jsx
+++ src/pages/WordTest.jsx
@@ -7,7 +7,9 @@ function WordTest() {
     <div className="flex flex-col min-h-[100dvh] bg-[var(--color-tertiary)] px-4 py-6 overflow-y-hidden" style={{ touchAction: 'pan-x' }}>
       <WordTestTimer />
       <div className="flex-grow flex items-center justify-center py-8">
-        <WordTestCardGallery />
+        <div className="w-screen ml-[-1rem] mr-[-1rem]">
+          <WordTestCardGallery />
+        </div>
       </div>
       <WordTestNavBar />
     </div>
