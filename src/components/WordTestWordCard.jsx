28e41aa0153e2bda5452a3a30134b4e49aa0ca8f
strangekid7822 <keji_zhaoqiang2006@163.com>
2025-07-14 15:22:15 +0800

    Style the inputs in word card.

--- src/components/WordTestWordCard.jsx
+++ src/components/WordTestWordCard.jsx
@@ -14,7 +14,7 @@ function WordTestWordCard({ wordLength = 6, cardData = {} }) {
             key={index}
             type="text"
             maxLength={1}
-            className="w-8 h-10 sm:w-7 sm:h-9 rounded-lg text-center text-base font-medium bg-[var(--color-tertiary)] focus:outline-none shadow-[var(--shadow-input)]"
+            className="w-8 h-10 sm:w-7 sm:h-9 rounded-lg text-center text-base font-medium bg-gray-100 border border-gray-300 focus:bg-white focus:outline-none"
           />
         ))}
       </div>
