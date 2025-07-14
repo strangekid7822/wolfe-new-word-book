146ba28656569a9071d6f17eaecf619df7d09d7a
strangekid7822 <keji_zhaoqiang2006@163.com>
2025-07-14 14:17:48 +0800

    Give the timer a white border.

--- src/components/WordTestTimer.jsx
+++ src/components/WordTestTimer.jsx
@@ -1,8 +1,10 @@
 function WordTestTimer() {
   return (
-    <div className="flex justify-center w-[var(--content-width)] mx-auto px-1.5">
-      <div className="bg-[var(--color-white)] rounded-full h-2.5 w-full shadow">
-        <div className="bg-[var(--color-secondary)] h-2.5 rounded-full ml-auto" style={{ width: "45%" }}></div>
+    <div className="flex justify-center">
+      <div className="bg-[var(--color-white)] rounded-full p-1 w-64">
+        <div className="bg-[var(--color-white)] rounded-full h-2.5">
+          <div className="bg-[var(--color-secondary)] h-2.5 rounded-full ml-auto" style={{ width: "45%" }}></div>
+        </div>
       </div>
     </div>
   );
