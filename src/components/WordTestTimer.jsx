c762f5c23d71854dc5d1c4bc523fbb3b6331f026
strangekid7822 <keji_zhaoqiang2006@163.com>
2025-07-14 14:28:55 +0800

    Give the timer a shadow.

--- src/components/WordTestTimer.jsx
+++ src/components/WordTestTimer.jsx
@@ -1,7 +1,7 @@
 function WordTestTimer() {
   return (
     <div className="flex justify-center">
-      <div className="bg-[var(--color-white)] rounded-full p-1 w-64">
+      <div className="bg-[var(--color-white)] rounded-full p-1 w-64 shadow-[var(--shadow-timer)]  w-[var(--content-width-2)]">
         <div className="bg-[var(--color-white)] rounded-full h-2.5">
           <div className="bg-[var(--color-secondary)] h-2.5 rounded-full ml-auto" style={{ width: "45%" }}></div>
         </div>
