3c21e2d4c0a410d67364f15613e104d59ea9b069
strangekid7822 <keji_zhaoqiang2006@163.com>
2025-07-14 15:44:48 +0800

    Active Style for All Icons in nav bar.

--- src/components/WordTestNavBar.jsx
+++ src/components/WordTestNavBar.jsx
@@ -7,13 +7,13 @@ function WordTestNavBar() {
 
   return (
     <div className="flex justify-around items-center h-13 w-[var(--content-width)] mx-auto bg-[var(--color-secondary)] rounded-full shadow-lg">
-      <NavLink to="/" className="p-2">
+      <NavLink to="/" className="p-2" style={({ isActive }) => isActive ? activeStyle : undefined}>
         <img src="/WordTestNavBar_HomeButton.svg" alt="Home" className="w-6 h-6" />
       </NavLink>
       <NavLink to="/write" className="p-2" style={({ isActive }) => isActive ? activeStyle : undefined}>
         <img src="/WordTestNavBar_WordCardButton.svg" alt="Write" className="w-6 h-6" />
       </NavLink>
-      <NavLink to="/profile" className="p-2">
+      <NavLink to="/profile" className="p-2" style={({ isActive }) => isActive ? activeStyle : undefined}>
         <img src="/WordTestNavBar_UserProfileButton.svg" alt="Profile" className="w-6 h-6" />
       </NavLink>
     </div>
