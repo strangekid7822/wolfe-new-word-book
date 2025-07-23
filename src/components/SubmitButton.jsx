6cc0d175d4a0516f65095e154245773a7f986106
strangekid7822 <keji_zhaoqiang2006@163.com>
2025-07-23 14:37:10 +0800

    Small fixes on the submit button logic.

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 
 const SubmitButton = ({ onClick, isDisabled, buttonText }) => {
   return (
-    <div className={`btn-animated-wrapper ${isDisabled ? 'border-transparent' : 'border-visible'}`}>
-      <button
-        onClick={onClick}
-        disabled={isDisabled}
-        className={isDisabled ? 'btn-disabled' : 'btn-animated'}
-      >
-        {buttonText}
-      </button>
-    </div>
+    <>
+      {isDisabled ? (
+        <button
+          onClick={onClick}
+          disabled={isDisabled}
+          className="btn-disabled"
+        >
+          {buttonText}
+        </button>
+      ) : (
+        <div className="btn-animated-wrapper border-visible">
+          <button
+            onClick={onClick}
+            className="btn-animated"
+          >
+            {buttonText}
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
